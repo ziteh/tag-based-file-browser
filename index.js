ce6df326989c3656997ca8a115fc1304c4e19756
ziteh <honmonoh@gmail.com>
2021-10-23 16:59:20 +0800

    Copy file from  ziteh / hierarchical-tag-file-browser

---
+++ index.js
@@ -0,0 +1,33 @@
+// Source: https://hackmd.io/@Heidi-Liu/note-be201-express-node#%E5%9C%A8-Nodejs-%E4%B8%8A%E5%AF%A6%E4%BD%9C-MVC-%E6%9E%B6%E6%A7%8B
+// Start with "nodemon index.js"
+
+const port = 5002;
+const express = require('express');
+const app = express();
+const db = require('./db')
+const tagController = require('./controllers/tag')
+
+// Middleware
+app.use(express.urlencoded({ extender: true }))
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.post('/', tagController.add)
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    tagsTree: 'tagsTree',
+    page: 'hello'
+  })
+})
+
+app.get('/tree',tagController.getTree)
+app.get('/tags', tagController.getAll)
+app.get('/tags/:id', tagController.get)
+
+app.listen(port, () => {
+  db.connect()
+  console.log(`Example app listening at http://localhost:${port}`)
+})
