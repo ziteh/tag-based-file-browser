0663b9cf224a93664bb6f5221fa4e5d313c98ebd
ziteh <honmonoh@gmail.com>
2021-10-31 21:43:59 +0800

    Fix the type for err

--- models/asyncTag.js
+++ models/asyncTag.js
@@ -5,8 +5,8 @@ const db = require('../db')
  */
 const getAll = () => new Promise((resolve, reject) => {
   db.query(
-    'SELECT * FROM tags', (err, results) => {
-      if (err) {
+    'SELECT * FROM tags', (error, results) => {
+      if (error) {
         reject(error);
         return;
       }
@@ -21,8 +21,8 @@ const getAll = () => new Promise((resolve, reject) => {
  */
 const get = (id) => new Promise((resolve, reject) => {
   db.query(
-    'SELECT * FROM tags WHERE id = ?', [id], (err, results) => {
-      if (err) {
+    'SELECT * FROM tags WHERE id = ?', [id], (error, results) => {
+      if (error) {
         reject(error);
         return;
       }
@@ -39,8 +39,8 @@ const getChildTags = (id) => new Promise((resolve, reject) => {
   const options =
     'SELECT * FROM tags INNER JOIN tag_relation ON tag_relation.child_tag_id = tags.id WHERE tag_relation.parent_tag_id = ?';
   db.query(
-    options, [id], (err, results) => {
-      if (err) {
+    options, [id], (error, results) => {
+      if (error) {
         reject(error);
         return;
       }
@@ -57,8 +57,8 @@ const getChildFiles = (id) => new Promise((resolve, reject) => {
   const options =
     'SELECT * FROM files INNER JOIN file_relation ON file_relation.child_file_id = files.id WHERE file_relation.parent_tag_id = ?';
   db.query(
-    options, [id], (err, results) => {
-      if (err) {
+    options, [id], (error, results) => {
+      if (error) {
         reject(error);
         return;
       }
